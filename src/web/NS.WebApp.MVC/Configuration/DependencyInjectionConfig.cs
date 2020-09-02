d25eca2beb0c680e522e36075bcd9cc68b95c1fb
santtose <santtose@outlook.com>
2020-09-02 11:48:56 -0300

    Aplicacao Web

---
+++ src/web/NS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NS.WebApp.MVC.Extensions;
+using NS.WebApp.MVC.Services;
+
+namespace NS.WebApp.MVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
+        }
+    }
+}
---
+++ src/web/NS.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -0,0 +1,11 @@
+﻿using NS.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NS.WebApp.MVC.Services
+{
+    public interface IAutenticacaoService
+    {
+        Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+        Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
+    }
+}
